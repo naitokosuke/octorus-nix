e130bb3fab6c003d8249dfbb884fea6ff6f20049
naitokosuke <102337893+naitokosuke@users.noreply.github.com>
2026-05-16 00:41:28 +0000

    feat: update octorus to v0.6.4

--- flake.nix
+++ flake.nix
@@ -16,13 +16,13 @@
 
       mkOctorus = pkgs: pkgs.rustPlatform.buildRustPackage rec {
         pname = "octorus";
-        version = "0.6.3";
+        version = "0.6.4";
 
         src = pkgs.fetchFromGitHub {
           owner = "ushironoko";
           repo = "octorus";
           rev = "v${version}";
-          hash = "sha256-RJNBCk4pop/aFzR1KFtP51nVfaWvfHmi07YWMlnFREM=";
+          hash = "sha256-xfbdT07/72LUHr6C3H07ImBYXG6QoRld0Jle7a4rfZA=";
         };
 
         cargoLock = {
